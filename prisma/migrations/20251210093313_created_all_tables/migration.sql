44e8fbd14584c43224f79489663d8e94eadbc54e
WesleyGoey <wesley.goey@gmail.com>
2025-12-10 16:33:55 +0700

    migrate

---
+++ prisma/migrations/20251210093313_created_all_tables/migration.sql
@@ -0,0 +1,90 @@
+-- CreateEnum
+CREATE TYPE "ReminderStatus" AS ENUM ('PENDING', 'DONE', 'MISSED');
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" SERIAL NOT NULL,
+    "name" VARCHAR(150) NOT NULL,
+    "email" VARCHAR(150) NOT NULL,
+    "password" VARCHAR(100) NOT NULL,
+    "phone" VARCHAR(50) NOT NULL,
+    "age" INTEGER NOT NULL,
+    "settingId" INTEGER,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "settings" (
+    "id" SERIAL NOT NULL,
+    "alarmSound" VARCHAR(100) NOT NULL DEFAULT 'default_alarm',
+    "notificationSound" VARCHAR(100) NOT NULL DEFAULT 'default_notification',
+
+    CONSTRAINT "settings_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "medicine" (
+    "id" SERIAL NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "name" VARCHAR(150) NOT NULL,
+    "type" VARCHAR(50) NOT NULL,
+    "dosage" VARCHAR(50) NOT NULL,
+    "frequency" VARCHAR(50),
+    "stock" INTEGER NOT NULL DEFAULT 0,
+    "minStock" INTEGER NOT NULL,
+    "notes" TEXT,
+    "image" VARCHAR(255),
+
+    CONSTRAINT "medicine_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "reminders" (
+    "id" SERIAL NOT NULL,
+    "medicineId" INTEGER NOT NULL,
+    "time" TIMESTAMP(3) NOT NULL,
+    "status" "ReminderStatus" NOT NULL DEFAULT 'PENDING',
+
+    CONSTRAINT "reminders_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "history" (
+    "id" SERIAL NOT NULL,
+    "reminderId" INTEGER NOT NULL,
+    "date" TIMESTAMP(3) NOT NULL,
+    "timeTaken" TIMESTAMP(3),
+
+    CONSTRAINT "history_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_settingId_key" ON "users"("settingId");
+
+-- CreateIndex
+CREATE INDEX "users_settingId_idx" ON "users"("settingId");
+
+-- CreateIndex
+CREATE INDEX "medicine_userId_idx" ON "medicine"("userId");
+
+-- CreateIndex
+CREATE INDEX "reminders_medicineId_idx" ON "reminders"("medicineId");
+
+-- CreateIndex
+CREATE INDEX "history_reminderId_idx" ON "history"("reminderId");
+
+-- AddForeignKey
+ALTER TABLE "users" ADD CONSTRAINT "users_settingId_fkey" FOREIGN KEY ("settingId") REFERENCES "settings"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "medicine" ADD CONSTRAINT "medicine_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "reminders" ADD CONSTRAINT "reminders_medicineId_fkey" FOREIGN KEY ("medicineId") REFERENCES "medicine"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "history" ADD CONSTRAINT "history_reminderId_fkey" FOREIGN KEY ("reminderId") REFERENCES "reminders"("id") ON DELETE CASCADE ON UPDATE CASCADE;
