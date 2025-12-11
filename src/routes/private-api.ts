d0032fbdc073bfe2b84415a67afb46590e57af22
nikigitdev <nmawardji@student.ciputra.ac.id>
2025-12-11 18:48:31 +0700

    reminder gajadi

--- src/routes/private-api.ts
+++ src/routes/private-api.ts
@@ -1,7 +1,6 @@
 import express from "express"
 import { authMiddleware } from "../middlewares/auth-middleware"
 import { MedicineController } from "../controllers/medicine-controller"
-import { ReminderController } from "../controllers/reminder-controller"
 import { SettingsController } from "../controllers/setting-controller"
 import { UserController } from "../controllers/user-controller"
 
@@ -21,11 +20,3 @@ privateRouter.get("/medicines/:medicineId", MedicineController.getMedicineById) 
 privateRouter.post("/medicines", MedicineController.addMedicine)
 privateRouter.patch("/medicines/:medicineId", MedicineController.updateMedicine)
 privateRouter.delete("/medicines/:medicineId", MedicineController.deleteMedicine)
-
-privateRouter.get("/reminders", ReminderController.getAllReminders)
-privateRouter.get("/reminders/upcoming", ReminderController.getUpcomingReminders)
-privateRouter.get("/reminders/:reminderId", ReminderController.getReminderById)
-privateRouter.get("/medicines/:medicineId/reminders", ReminderController.getRemindersByMedicine) //p
-privateRouter.post("/reminders", ReminderController.addReminder)
-privateRouter.patch("/reminders/:reminderId", ReminderController.updateReminder)
-privateRouter.delete("/reminders/:reminderId", ReminderController.deleteReminder)
